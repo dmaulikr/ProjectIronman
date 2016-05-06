7ea20b0ccabd4d1aae4f4ed760c08079b2777b93
Jason Cheng <yannru.cheng@gmail.com>
2016-05-06 12:50:42 -0700

    adding image resources

--- ProjectIronman/FriendTableViewCell.swift
+++ ProjectIronman/FriendTableViewCell.swift
@@ -14,6 +14,17 @@ class FriendTableViewCell: UITableViewCell {
     @IBOutlet weak var friendName: UILabel!
     @IBOutlet weak var buttonLeft: UIButton!
     @IBOutlet weak var buttonRight: UIButton!
+
+    @IBAction func leftButtonTapped(sender: UIButton) {
+        buttonLeftAction?(button: sender)
+    }
+    
+    @IBAction func rightButtonTapped(sender: UIButton) {
+        buttonRightAction?(button: sender)
+    }
+    
+    private var buttonLeftAction: ((button: UIButton) -> Void)?
+    private var buttonRightAction: ((button: UIButton) -> Void)?
     
     
     override func awakeFromNib() {
@@ -27,10 +38,31 @@ class FriendTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    // friends
+    // find friends
+    // friend requests
     
-    
-    
-    func populateWithData(){
+    func setUpBasicFriendCell(){
+        buttonLeft.hidden = true
+        buttonRight.hidden = true
     }
-
+    
+    func setUpFindFriendCell(addFriend: (button:UIButton) -> Void){
+        buttonLeft.hidden = true
+        buttonRight.hidden = false
+        buttonRight.setImage(UIImage(named: "addUser.png"), forState: .Normal)
+        self.buttonRightAction = addFriend
+    }
+    
+    func setUpFriendRequestCell(decline: (button:UIButton) -> Void,
+                                accept: (button:UIButton) -> Void){
+        self.buttonLeftAction = decline
+        self.buttonRightAction = accept
+        
+        buttonLeft.hidden = false
+        buttonRight.hidden = false
+        
+        buttonRight.setImage(UIImage(named: "accept.png"), forState: .Normal)
+        buttonLeft.setImage(UIImage(named: "decline.png"), forState: .Normal)
+    }
 }
